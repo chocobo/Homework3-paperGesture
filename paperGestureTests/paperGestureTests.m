2c38e0d6507d43b2b141a066622e7f46f11045fd
chocobo <nacf@nacf-macbookpro2.roam.corp.google.com>
2014-07-06 08:40:17 -0700

    Initial Commit

---
+++ paperGestureTests/paperGestureTests.m
@@ -0,0 +1,34 @@
+//
+//  paperGestureTests.m
+//  paperGestureTests
+//
+//  Created by Natalia Fisher on 7/6/14.
+//  Copyright (c) 2014 Google. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface paperGestureTests : XCTestCase
+
+@end
+
+@implementation paperGestureTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
